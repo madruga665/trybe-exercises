fcf7f5b07d7dbc8e4ec74e4f367cd0ba9e199851
madruga665 <madruga665@hotmail.com>
2021-02-25 11:02:39 -0300

    prog06 - verifica o maior entre 2 numeros

---
+++ exercises/bloco-4/4.1/ex5/prog06.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 5;
+
+if (a > b) {
+  console.log(`${a} é maior que ${b}`);
+} else if (a < b) {
+  console.log(`${b} é maior que ${a}`);
+} else {
+  console.log(`Numeros iguais`);
+}
