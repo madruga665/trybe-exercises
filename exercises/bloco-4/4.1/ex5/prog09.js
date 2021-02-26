99ffb8317362bc2cc3d9b6a7fd98ea3e6b4a574b
madruga665 <madruga665@hotmail.com>
2021-02-26 08:14:32 -0300

    prog09 - add msg de error em numeros negativos

--- exercises/bloco-4/4.1/ex5/prog09.js
+++ exercises/bloco-4/4.1/ex5/prog09.js
@@ -1,11 +1,12 @@
 let angulo1 = 100;
 let angulo2 = 40;
-let angulo3 = 4;
+let angulo3 = 40;
 
 let soma = angulo1 + angulo2 + angulo3 == 180;
 
 if (soma == true) {
   console.log(`É um triangulo? ${soma}`);
-} else {
-  console.log(`É um triangulo? ${soma}`);
+} else if ( soma <= 0) {
+  console.log(`${soma}`)
+  console.log(`ERROR`);
 }
