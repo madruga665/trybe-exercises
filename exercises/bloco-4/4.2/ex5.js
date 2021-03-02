f979b5a8d37605960105ee3e57b7e681cbcc3230
madruga665 <madruga665@hotmail.com>
2021-03-02 01:05:18 -0300

    ex5 descobre qual o maior numero do array

---
+++ exercises/bloco-4/4.2/ex5.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = 0;
+
+for (let index = 0; index < numbers.length; index = index + 1) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
+}
+
+console.log(higherNumber);
+
