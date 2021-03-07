96202fe1d041ae5062ba348ca19ae93a7828ac29
madruga665 <madruga665@hotmail.com>
2021-03-07 19:34:33 -0300

    ex1 e ex2

---
+++ exercises/bloco-4/4.3/exercicios/ex1.js
@@ -0,0 +1,9 @@
+let n = 5;
+let line = "";
+let asterisk = "*";
+for (let index = 0; index < n; index += 1) {
+  line += asterisk;
+}
+for (let index = 0; index < n; index += 1) {
+  console.log(line);
+}
