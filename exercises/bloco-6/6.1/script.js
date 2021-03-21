8e35dd63da8fdd33342c05d60f8aded1e551712f
madruga665 <madruga665@hotmail.com>
2021-03-21 04:54:29 -0300

    a

--- exercises/bloco-6/6.1/script.js
+++ exercises/bloco-6/6.1/script.js
@@ -1,13 +1,15 @@
-function generateStates() {
-  let states = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
-  const state = document.getElementById('state');
+// script.js
+const states = document.getElementById('state');
+function createStateOptions() {
+  const stateOptions = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 
-  for (let index = 0; index < states.length; index += 1) {
-    const createOptions = document.createElement("option");
-    state.appendChild(createOptions).innerHTML = states[index];
-    state.appendChild(createOptions).value = states[index];
+  for (let index = 0; index < stateOptions.length; index += 1) {
+    const createOptions = document.createElement('option');
+    states.appendChild(createOptions).innerText = stateOptions[index];
+    states.appendChild(createOptions).value = stateOptions[index];
   }
 }
-window.onload = () => {
-  generateStates();
-};
+
+window.onload = function () {
+  createStateOptions();
+}
