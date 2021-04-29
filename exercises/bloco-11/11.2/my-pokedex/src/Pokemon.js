55763d23441cce3064dce5d9a780c9cf706fbca8
madruga665 <madruga665@hotmail.com>
2021-04-29 10:42:14 -0300

    pokedex refatorada

--- exercises/bloco-11/11.2/my-pokedex/src/Pokemon.js
+++ exercises/bloco-11/11.2/my-pokedex/src/Pokemon.js
@@ -4,7 +4,8 @@ import PropTypes from "prop-types";
 
 class Pokemon extends React.Component {
   render() {
-    const { name, type, averageWeight, image } = this.props.pokemon;
+    const { pokemon } = this.props;
+    const { name, type, averageWeight, image } = pokemon;
     return (
       <div className="pokemon">
         <div>
