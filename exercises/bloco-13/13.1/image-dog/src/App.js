8287d20700323a1696d245def42eb3d42e81cec5
madruga665 <madruga665@hotmail.com>
2021-05-07 10:21:11 -0300

    Exercício 1 bloco 13.1 exercicio do cão

---
+++ exercises/bloco-13/13.1/image-dog/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: "",
+    };
+  }
+  fetchDog = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await response.json();
+    this.setState({ data: data });
+  };
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  render() {
+    const { data } = this.state;
+    const showLoading = <span>Loading...👹️</span>;
+    const imageDog = <img width="400" src={data.message} alt="Imagem do Cão" />;
+
+    return (
+      <div className="App">
+        <div>
+          <h1>Aqui você vai ver o Cão!</h1>
+          {data === "" ? showLoading : imageDog}
+        </div>
+        <button onClick={this.fetchDog}>Novo Cão!</button>
+      </div>
+    );
+  }
+}
+
+export default App;
