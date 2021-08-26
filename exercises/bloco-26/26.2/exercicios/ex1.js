cb12390c35713d1720d245ce987f2e0a788443d4
Luciano Amâncio <madruga665@hotmail.com>
2021-08-26 19:27:19 -0300

    bloco 26.2 exercicio 2

--- exercises/bloco-26/26.2/exercicios/ex1.js
+++ exercises/bloco-26/26.2/exercicios/ex1.js
@@ -13,6 +13,29 @@ const funcaoComPromise = (a, b, c) => {
   return promise;
 };
 
-funcaoComPromise(5, 5, 5)
+// funcaoComPromise(5, 5, 5)
+//   .then((resolve) => console.log(resolve))
+//   .catch((error) => console.log(error.message));
+
+const randomNumbers = () => {
+  return Math.floor(Math.random() * 100 + 1);
+};
+
+// GABARITO
+
+// function callDoMath() {
+// Criamos um novo array de 3 posições
+// e utilizamos o `map` para gerar um número aleatório
+// para cada posição do Array
+//   const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+// }
+
+// MEU JEITO BURRO DE FAZER AS COISAS
+
+const a = randomNumbers();
+const b = randomNumbers();
+const c = randomNumbers();
+
+funcaoComPromise(a, b, c)
   .then((resolve) => console.log(resolve))
   .catch((error) => console.log(error.message));
